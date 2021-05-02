eb4a95138b63ba21f4851cfc32ca568951964635
Emilio Dalla Torre <info@emiliodallatorre.it>
2021-05-02 17:51:20 +0200

    Aggiunta la visualizzazione dell'estratto.

--- includes/template_widget.php
+++ includes/template_widget.php
@@ -5,9 +5,15 @@ if (have_posts()) {
 	$output .= '<ol>';
 	while (have_posts()) {
 		the_post();
+		
+		$excerpt = strip_tags((string) get_the_excerpt());
+		preg_replace('/([,;.-]+)\s*/','\1 ', $excerpt);
+		$excerpt = implode(' ', array_slice(preg_split('/\s+/',$excerpt), 0, $excerpt_length)).'...';
+		
 		$output .= '<li><a href="'.get_permalink().'" rel="bookmark">'.get_the_title().'</a>';
 //		$output .= ' ('.round(get_the_score(),3).')';
 		$output .= '</li>';
+		$output .= '<br>'.$excerpt;
 	}
 	$output .= '</ol>';
 } else {
